7ccaab8dbd33806759bce977c7596d9d177f4b85
thenoseman <frank.schumacher.privat@gmail.com>
2019-09-25 21:08:36 +0200

    install macvim

--- install-with-rbenv.sh
+++ install-with-rbenv.sh
@@ -3,7 +3,11 @@
 # Install macvim with activated rbenv ruby
 #
 HERE=$(pwd)
-sed -E -i '' "s|--enable-rubyinterp\",|--enable-rubyinterp\", \"--with-ruby-command=$(rbenv which ruby)\",|" /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core/Formula/macvim.rb
+brew unlink macvim || true
+RUBY="$(rbenv which ruby)"
+echo "Building macvim with $RUBY"
+sed -E -i '' "s|--enable-rubyinterp\",|--enable-rubyinterp\", \"--with-ruby-command=$RUBY\",|" /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core/Formula/macvim.rb
+brew install --force --build-from-source macvim
 cd /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core || exit 1
 git reset --hard
 cd "$HERE" || true
